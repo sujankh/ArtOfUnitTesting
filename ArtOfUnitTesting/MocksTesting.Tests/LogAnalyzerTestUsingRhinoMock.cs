aafeef1caddefcf2e53c85a8375ff467f0cb68d6
sujankh <kcsujan@gmail.com>
2014-02-22 19:53:48 +0545

    Added Tests using Rhinomock

---
+++ ArtOfUnitTesting/MocksTesting.Tests/LogAnalyzerTestUsingRhinoMock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MocksTesting.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTestUsingRhinoMock
+    {
+        [Test]
+        public void Analyze_ShortFileName_LogsError()
+        {
+            var mocks = new MockRepository();
+            var mockWebService = mocks.StrictMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                mockWebService.LogError("Short FileName:abc.txt");
+            }
+
+            var logAnalyzer = new LogAnalyzer
+            {
+                WebService = mockWebService
+            };
+
+            logAnalyzer.Analyze("abc.txt");
+
+            mocks.Verify(mockWebService);
+        }
+    }
+}
+
